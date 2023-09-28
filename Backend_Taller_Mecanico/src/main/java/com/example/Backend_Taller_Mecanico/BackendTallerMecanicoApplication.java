6fe6285685bd40cd2f6bc45c4493f8ba98f5d1c2
Tomas Zegatti <109703867+TomiZega@users.noreply.github.com>
2023-09-27 22:40:36 -0300

    CRUD Vehiculo (tiene un error raro y no anda)
    no tengo idea por que no anda

---
+++ Backend_Taller_Mecanico/src/main/java/com/example/Backend_Taller_Mecanico/BackendTallerMecanicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Taller_Mecanico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTallerMecanicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTallerMecanicoApplication.class, args);
+	}
+
+}
---
+++ Backend_Taller_Mecanico/src/test/java/com/example/Backend_Taller_Mecanico/BackendTallerMecanicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Taller_Mecanico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTallerMecanicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
