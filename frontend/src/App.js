807652740504ad4403552d36afa03b84be7aadc1
sebastianMosca <sebastian.mosca@outlook.com>
2023-10-02 19:48:38 -0300

    Construccion y refinamiento de contexto de CRUD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,22 +9,25 @@ import Cliente from './pages/Cliente';
 import Orden from './pages/Orden';
 import Home from './pages/Home';
 import Drawer from './components/Drawer';
+import CrudContextProvider from './contexts/CrudContext'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Drawer/>
-      <Routes>
-      <Route path={"/"} element={<Home />} />
-      <Route path={"/marca"} element={<Marca />} />
-      <Route path={"/modelo"} element={<Modelo />} />
-      <Route path={"/tecnico"} element={<Tecnico />} />
-      <Route path={"/vehiculo"} element={<Vehiculo />} />
-      <Route path={"/cliente"} element={<Cliente />} />
-      <Route path={"/orden"} element={<Orden />} />
-      </Routes>
-      </Router>
+      <CrudContextProvider>
+        <Router>
+          <Drawer />
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/marca"} element={<Marca />} />
+            <Route path={"/modelo"} element={<Modelo />} />
+            <Route path={"/tecnico"} element={<Tecnico />} />
+            <Route path={"/vehiculo"} element={<Vehiculo />} />
+            <Route path={"/cliente"} element={<Cliente />} />
+            <Route path={"/orden"} element={<Orden />} />
+          </Routes>
+        </Router>
+      </CrudContextProvider>
     </div>
   );
 }
