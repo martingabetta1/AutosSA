acb43674eb189a9c8d11d660c2d2c3dc61781038
TomiZega <tomaszegatti1@gmail.com>
2023-12-08 23:36:19 -0300

    filtros Vehiculo [NO ANDA]
    filtro de vehiculo (NO ANDA NO PUDE HACER FILTRO CON CLAVES FORANEAS PARA MODELO Y CLIENTE)

--- API-Taller-Mecanico/src/main/java/com/example/API/Taller/Mecanico/controller/VehiculoController.java
+++ API-Taller-Mecanico/src/main/java/com/example/API/Taller/Mecanico/controller/VehiculoController.java
@@ -20,21 +20,44 @@ public class VehiculoController {
 
     @Autowired
     IClienteService serviceCliente;
-
+    // pantente string, observaciones string, anio integer, kilometros float, cliente cliente, modelo modelo
     @GetMapping
     public ResponseEntity<List<Vehiculo>> listarVehiculos(
-            @RequestParam(name = "select", required = false, defaultValue = "false") boolean select) {
+            @RequestParam(name = "select", required = false, defaultValue = "false") boolean select,
+            @RequestParam(name = "patente", required = false) String patente,
+            @RequestParam(name = "observaciones", required = false) String observaciones,
+            @RequestParam(name = "anio", required = false) Integer anio,
+            @RequestParam(name = "kilometros", required = false) Float kilometros,
+            @RequestParam(name = "cliente", required = false) Cliente cliente,
+            @RequestParam(name = "modelo", required = false) Modelo modelo) {
         List<Vehiculo> vehiculos = serviceVehiculo.listarVehiculos();
-        for (Vehiculo vehiculo : vehiculos) {
-            Modelo modelo = new Modelo();
-            modelo.setId(vehiculo.getModelo().getId());
-            modelo.setDescripcion(vehiculo.getModelo().getNombre());
-            vehiculo.setModelo(modelo);
-            Cliente cliente = new Cliente();
-            cliente.setId(vehiculo.getCliente().getId());
-            cliente.setDescripcion(vehiculo.getCliente().getNombre() + ' ' + vehiculo.getCliente().getApellido());
-            vehiculo.setCliente(cliente);
-        }
+        if(patente != null || observaciones != null || anio != null || kilometros != null || cliente != null || modelo != null){
+            List<Vehiculo> buscarVehiculos = serviceVehiculo.listarVehiculosPorConsultaAnidada(patente, observaciones, anio, kilometros, cliente, modelo);
+            List<Vehiculo> vehiculosConFiltro = new ArrayList<>();
+            for(Vehiculo vehiculo : buscarVehiculos){
+                Vehiculo vehiculoFiltrado = new Vehiculo();
+                vehiculoFiltrado.setId(vehiculo.getId());
+                vehiculoFiltrado.setPatente(vehiculo.getPatente());
+                vehiculoFiltrado.setObservaciones(vehiculo.getObservaciones());
+                vehiculoFiltrado.setAnio(vehiculo.getAnio());
+                vehiculoFiltrado.setKilometros(vehiculo.getKilometros());
+                vehiculoFiltrado.setCliente(vehiculo.getCliente());
+                vehiculoFiltrado.setModelo(vehiculo.getModelo());
+                vehiculosConFiltro.add(vehiculoFiltrado); 
+            }
+            return new ResponseEntity<List<Vehiculo>>(vehiculosConFiltro, HttpStatus.OK);
+        };
+
+//        for (Vehiculo vehiculo : vehiculos) {
+//            Modelo modelo = new Modelo();
+//            modelo.setId(vehiculo.getModelo().getId());
+//            modelo.setDescripcion(vehiculo.getModelo().getNombre());
+//            vehiculo.setModelo(modelo);
+//            Cliente cliente = new Cliente();
+//            cliente.setId(vehiculo.getCliente().getId());
+//            cliente.setDescripcion(vehiculo.getCliente().getNombre() + ' ' + vehiculo.getCliente().getApellido());
+//            vehiculo.setCliente(cliente);
+//        }
 
         if (select) {
             // Si select es true, formatear la respuesta con el formato deseado
