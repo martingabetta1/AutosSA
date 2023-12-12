faa941398e8372f87340143326d59026037ea643
martingabetta1 <105307200+martingabetta1@users.noreply.github.com>
2023-12-12 17:49:55 -0300

    Agregar estadística promedio técnicos

---
+++ API-Taller-Mecanico/src/main/java/com/example/API/Taller/Mecanico/dto/TecnicosEstadisticaDTO.java
@@ -0,0 +1,9 @@
+package com.example.API.Taller.Mecanico.dto;
+
+import lombok.Data;
+
+@Data
+public class TecnicosEstadisticaDTO {
+    private String nombreTecnico;
+    private Integer diasPromedio;
+}
