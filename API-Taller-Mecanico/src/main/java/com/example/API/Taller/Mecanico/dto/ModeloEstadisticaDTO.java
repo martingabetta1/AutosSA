7d9cbb35cbec3ca2272e73342b83d19fd8dabd8e
martingabetta1 <105307200+martingabetta1@users.noreply.github.com>
2023-12-12 16:09:05 -0300

    Agregar estadistica

---
+++ API-Taller-Mecanico/src/main/java/com/example/API/Taller/Mecanico/dto/ModeloEstadisticaDTO.java
@@ -0,0 +1,12 @@
+package com.example.API.Taller.Mecanico.dto;
+
+import lombok.Data;
+
+@Data
+public class ModeloEstadisticaDTO {
+    private String nombreModelo;
+    private Integer cantidadOrdenes;
+
+
+
+}
--- API-Taller-Mecanico/src/main/resources/import.sql
+++ API-Taller-Mecanico/src/main/resources/import.sql
@@ -40,9 +40,25 @@ INSERT INTO cliente (id, nombre, apellido, direccion, telefono, mail, localidad,
 
 INSERT INTO cliente (id, nombre, apellido, direccion, telefono, mail, localidad, eliminado) VALUES (4, 'Juan', 'López','Street 123',111111,'juanlopez@gmail.com','Buenos Aires',false);
 
+-- Inserción en la tabla 'Vehiculo'
+INSERT INTO vehiculo (id, patente, id_modelo, id_cliente, anio, kilometros, observaciones, eliminado) VALUES (0, 'ABC123', 0, 2, 2020, 100, 'Observacion', false);
+INSERT INTO vehiculo (id, patente, id_modelo, id_cliente, anio, kilometros, observaciones, eliminado) VALUES (1, 'XYZ789', 1, 2, 2020, 100, 'Observacion', false);
+INSERT INTO vehiculo (id, patente, id_modelo, id_cliente, anio, kilometros, observaciones, eliminado) VALUES (2, 'DEF456', 2, 2, 2020, 100, 'Observacion', false);
+INSERT INTO vehiculo (id, patente, id_modelo, id_cliente, anio, kilometros, observaciones, eliminado) VALUES (3, 'GHI789', 3, 2, 2020, 100, 'Observacion', false);
+INSERT INTO vehiculo (id, patente, id_modelo, id_cliente, anio, kilometros, observaciones, eliminado) VALUES (4, 'JKL012', 4, 2, 2020, 100, 'Observacion', false);
+INSERT INTO vehiculo (id, patente, id_modelo, id_cliente, anio, kilometros, observaciones, eliminado) VALUES (5, 'AAA222', 5, 2, 2020, 100, 'Observacion', false);
+
+
 -- Inserción en la tabla 'tecnico'
 INSERT INTO tecnico (id, apellido, direccion, documento, nombre, telefono, eliminado) VALUES (0, 'Doffo', 'Gomez 123', '30432312', 'Marcelo', '3143412424', false);
 
 INSERT INTO tecnico (id, apellido, direccion, documento, nombre, telefono, eliminado) VALUES (1, 'Achetta', 'Hipolito Yrigoyen 321', '42312312', 'Mauricio', '3221312314', false);
 
 INSERT INTO tecnico (id, apellido, direccion, documento, nombre, telefono, eliminado) VALUES (2, 'Simonin', 'Yugusman 999', '41331231', 'Eloy', '321312634', false);
+
+-- Inserción en la tabla 'orden_trabajo'
+INSERT INTO orden_trabajo (id, fecha_inicio, fecha_fin, id_vehiculo, id_tecnico, id_estado, comentario, totalcosto, id_cliente, eliminado) VALUES (0, '2023-12-12', '2023-12-15', 0, 0, 0, 'Comentario 1', 100.0, 0, false);
+INSERT INTO orden_trabajo (id, fecha_inicio, fecha_fin, id_vehiculo, id_tecnico, id_estado, comentario, totalcosto, id_cliente, eliminado) VALUES (1, '2023-12-10', '2023-12-14', 1, 1, 1, 'Comentario 2', 150.0, 1, false);
+INSERT INTO orden_trabajo (id, fecha_inicio, fecha_fin, id_vehiculo, id_tecnico, id_estado, comentario, totalcosto, id_cliente, eliminado) VALUES (2, '2023-12-11', '2023-12-13', 1, 2, 2, 'Comentario 3', 120.0, 2, false);
+INSERT INTO orden_trabajo (id, fecha_inicio, fecha_fin, id_vehiculo, id_tecnico, id_estado, comentario, totalcosto, id_cliente, eliminado) VALUES (3, '2023-12-14', '2023-12-16', 1, 0, 3, 'Comentario 4', 200.0, 3, false);
+INSERT INTO orden_trabajo (id, fecha_inicio, fecha_fin, id_vehiculo, id_tecnico, id_estado, comentario, totalcosto, id_cliente, eliminado) VALUES (4, '2023-12-13', '2023-12-17', 3, 1, 0, 'Comentario 5', 180.0, 4, false);
