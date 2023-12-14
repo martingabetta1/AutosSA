5ca94cba88fa7ba894f269afe08e55d5cf22d571
simonineloy123 <simonineloy123@gmail.com>
2023-12-13 23:57:23 -0300

    Se completan tests de controlador para Marca y tests para el Servicio de Marca

--- API-Taller-Mecanico/src/test/java/com/example/API/Taller/Mecanico/controller/MarcaControllerTest.java
+++ API-Taller-Mecanico/src/test/java/com/example/API/Taller/Mecanico/controller/MarcaControllerTest.java
@@ -5,6 +5,7 @@ import com.example.API.Taller.Mecanico.service.IMarcaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,7 +17,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.Arrays;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MarcaController.class)
@@ -30,6 +31,9 @@ class MarcaControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private MarcaController marcaController;
 
     @Test
     @DisplayName("Test para validar correctamente la ruta del endpoint y devuelve un estado 200 al listar marcas sin campo select")
@@ -92,5 +96,43 @@ class MarcaControllerTest {
 
     }
 
+    @Test
+    @DisplayName("Test para actualizar marca")
+    void testActualizarMarca() throws Exception {
 
+        Marca marca = new Marca();
+        marca.setId(1);
+        marca.setNombre("BMW");
+
+        doNothing().when(serviceMarca).actualizar(eq(marca.getId()), eq(marca.getNombre()));
+
+        mvc.perform(MockMvcRequestBuilders.put("/marcas/actualizar/{id}", marca.getId())
+           .contentType(MediaType.APPLICATION_JSON)
+           .content(objectMapper.writeValueAsString(marca)))
+           .andExpect(status().isOk())
+           .andExpect(content().string("La marca se actualizó correctamente"));
+
+
+        //Verifico que se llamo bien el servicio:
+        verify(serviceMarca, times(1)).actualizar(eq(marca.getId()), eq(marca.getNombre()));
+
+    }
+
+    @Test
+    @DisplayName("Test para eliminar marca")
+    void testEliminarMarca() throws Exception{
+
+        Marca marca = new Marca();
+        marca.setId(1);
+        marca.setNombre("Toyota");
+
+        doNothing().when(serviceMarca).eliminar(eq(marca.getId()));
+
+        mvc.perform(MockMvcRequestBuilders.post("/marcas/eliminar/{id}", marca.getId())
+           .contentType(MediaType.APPLICATION_JSON))
+           .andExpect(status().isOk());
+
+        verify(serviceMarca, times(1)).eliminar(eq(marca.getId()));
+
+    }
 }
